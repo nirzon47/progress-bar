4d7a296ba6a64d57d750da58bf4c696746a03f70
Nirzon Karmakar <nirzon47@gmail.com>
2023-12-28 12:11:31 +0530

    added confetti when progress reaches 100%

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import ProgressBar from './ProgressBar'
 import { useEffect, useState } from 'react'
 
+import confetti from 'canvas-confetti'
+
 const App = () => {
 	const [progress, setProgress] = useState(0)
 
+	const triggerConfetti = () => {
+		confetti({
+			particleCount: 100,
+			spread: 70,
+			origin: { y: 0.6 },
+		})
+	}
+
 	useEffect(() => {
 		if (progress >= 100) {
+			triggerConfetti()
 			return
 		}
 
@@ -27,7 +38,7 @@ const App = () => {
 		<div className='grid place-content-center h-screen'>
 			<h1 className='text-center font-medium text-xl mb-8'>Progress Bar</h1>
 			<ProgressBar progress={progress} />
-			<p className='text-center'>
+			<p className='text-center mb-4'>
 				{progress <= 100
 					? `Progress: ${progress.toFixed(0)}%`
 					: 'Loading Complete!'}
